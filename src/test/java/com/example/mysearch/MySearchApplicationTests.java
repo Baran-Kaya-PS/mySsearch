e646c18f86d7d81b9fd31950e27d7f3467858617
admin <baran.kaya@etu.iut-tlse3.fr>
2024-01-04 17:19:47 +0100

    test tfidfScoreForBreakingBadShouldBeHighestForMeth working

--- src/test/java/com/example/mysearch/MySearchApplicationTests.java
+++ src/test/java/com/example/mysearch/MySearchApplicationTests.java
@@ -2,11 +2,16 @@ package com.example.mysearch;
 
 import com.example.mysearch.model.Serie;
 import com.example.mysearch.repository.SerieRepository;
+import com.example.mysearch.utils.TFIDFCalculator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -14,6 +19,8 @@ public class MySearchApplicationTests {
 
     @Autowired // injecte le bean dans le test
     private SerieRepository serieRepository;
+    @Autowired
+    private TFIDFCalculator tfidfCalculator;
 
     @Test
     public void shouldFetchData() {
@@ -25,5 +32,18 @@ public class MySearchApplicationTests {
 
         assertThat(series).isNotEmpty();
         assertThat(series.get(0).getTitre()).isEqualTo(expectedTitle);
+    }
+    @Test
+    public void tfidfScoreForBreakingBadShouldBeHighestForMeth() {
+        // Given
+        String keyword = "meth";
+        String expectedTopTitle = "breakingbad";
+
+        // When
+        List<Map<String, Object>> topSeries = tfidfCalculator.findTopSeries(Collections.singletonList(keyword), 10);
+
+        // Then
+        assertThat(topSeries).isNotEmpty();
+        assertThat(topSeries.get(0)).containsEntry("title", expectedTopTitle);
     }
 }
